e101fa374c14a46e8ce94e832d5cc2b4efecbc57
Shekhar Gulati <shekhargulati84@gmail.com>
2014-05-12 03:59:16 +0530

    using Tornado

--- app.py
+++ app.py
@@ -10,12 +10,23 @@ except IOError:
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-
-from gevent.wsgi import WSGIServer
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 from hello import app
 
+http_server = HTTPServer(WSGIContainer(app))
 ip   = os.environ['OPENSHIFT_PYTHON_IP']
 port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
 
-http_server = WSGIServer((ip, port), app)
-http_server.serve_forever()
+http_server.listen(port, ip)
+IOLoop.instance().start()
+
+# from gevent.wsgi import WSGIServer
+# from hello import app
+
+# ip   = os.environ['OPENSHIFT_PYTHON_IP']
+# port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+
+# http_server = WSGIServer((ip, port), app)
+# http_server.serve_forever()
